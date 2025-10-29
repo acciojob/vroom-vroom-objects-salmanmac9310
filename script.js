1635847e79a6e6c710360c45b766e80460957f53
Salman mac <salmanmac168@gmail.com>
2025-10-30 00:41:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make= make,
+    this.model = model
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	super(make,model)
+	this.topSpeed = topSpeed
+}
 
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
+
+
+
