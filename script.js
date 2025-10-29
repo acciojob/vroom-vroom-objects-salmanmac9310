11b2fe664ebe0ef942ff8419658098e9824d47b0
Salman mac <salmanmac168@gmail.com>
2025-10-30 00:43:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ function SportsCar(make, model, topSpeed) {
 	this.topSpeed = topSpeed
 }
 
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel()); // Output: Ferrari Testarossa
+console.log(car.getTopSpeed()); // Output: 200
+
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
